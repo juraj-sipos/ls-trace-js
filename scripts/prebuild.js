306db0bc5b2088fff51eca8a3224fbb5944f4546
Juraj Sipos <jsipos@newstore.com>
2022-11-11 11:23:05 +0100

    bumping node version

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -22,7 +22,10 @@ const targets = [
   { version: '12.0.0', abi: '72' },
   { version: '13.0.0', abi: '79' },
   { version: '14.0.0', abi: '83' },
-  { version: '15.0.0', abi: '88' }
+  { version: '15.0.0', abi: '88' },
+  { version: '16.0.0', abi: '93' },
+  { version: '17.0.0', abi: '102' },
+  { version: '18.0.0', abi: '108' }
 ].filter(target => semver.satisfies(target.version, NODE_VERSIONS))
 
 prebuildify()
